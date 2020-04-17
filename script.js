d99bb9c35738601aa5c98ae8f93c47505f1865b5
Jelhill <talk2jelili2000@gmail.com>
2020-04-17 13:05:44 +0100

    404 page

--- script.js
+++ script.js
@@ -4,12 +4,14 @@
 
 
 
-document.addEventListener("click", function(e) {
+document.getElementById("login").addEventListener("click", function(e) {
     e.preventDefault()
     let email = document.getElementById('email').value;
     let password = document.getElementById("password").value
 
     if(email === "email@email.com" && password === "password" ){
         location.replace("homepage.html")
-    } 
+    }else{
+        location.replace("404.html")
+    }
 })
